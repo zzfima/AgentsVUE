3b103023dcde8d93d36a7a4287dfdb6930bf0588
zzfima <zzfima@gmail.com>
2020-10-11 14:11:39 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Version />
+  </div>
+</template>
+
+<script>
+import Version from "./components/Version";
+
+export default {
+  name: "App",
+  components: {
+    Version,
+  },
+};
+</script>
